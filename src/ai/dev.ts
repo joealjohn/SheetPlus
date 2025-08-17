0f6977873f51b79e97df27d58e1678cf377a8573
JOEAL JOHN <joealjohn290@gmail.com>
2025-08-17 13:45:30 +0000

    https://sheet-plus.vercel.app/
    how to fix

--- src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-'use server';
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/enhance-data-interpretation.ts';
-import '@/ai/flows/auto-correct-ocr.ts';
-import '@/ai/flows/extract-text-from-image.ts';
